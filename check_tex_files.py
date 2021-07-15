c374d0fe7245e62394447dd080cb814a7a90aae8
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-07-15 23:18:36 +0900

    pre-commit を導入

--- check_tex_files.py
+++ check_tex_files.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
 
-import pathlib
-import logging
-import re
-
-logging.basicConfig(level=logging.INFO)
-
-LOGGER = logging.getLogger(__file__)
+from sys import argv
 
 ENCODING = 'utf8'
-SUCCESS = True
 
 
 def fix_ten_maru(filepath: str):
@@ -24,57 +17,13 @@ def fix_ten_maru(filepath: str):
 
     fixed_contents = contents.replace('、', '，').replace('。', '．')
     if contents != fixed_contents:
-        LOGGER.warning('fixed ten maru in a file %s', filepath)
-        global SUCCESS
-        SUCCESS = False
+        print(f'Fixing {filepath}')
 
     with open(filepath, mode='w', encoding=ENCODING) as file:
         file.write(fixed_contents)
 
 
-def check_file(filepath: pathlib.Path):
-    """ファイルをチェックする
-
-    Args:
-        filepath (pathlib.Path): ファイルパス
-    """
-
-    LOGGER.debug('check a file %s', filepath)
-    fix_ten_maru(str(filepath))
-
-
-IGNORE_DIR_REGEX = re.compile(R'\.git|extern')
-TEX_FILE_SUFFIX = '.tex'
-
-
-def fix_in_dir(dirpath: pathlib.Path):
-    """ディレクトリ内のファイルを修正する
-
-    Args:
-        dirpath (pathlib.Path): ディレクトリパス
-    """
-
-    LOGGER.debug('search in directory %s', dirpath)
-    for child in dirpath.iterdir():
-        if child.is_dir() and not IGNORE_DIR_REGEX.search(str(child)):
-            fix_in_dir(child)
-        if child.is_file() and str(child).endswith(TEX_FILE_SUFFIX):
-            check_file(child)
-
-
-def fix_all():
-    """全ファイルを修正する
-    """
-
-    this_dir = pathlib.Path(__file__).parent
-    fix_in_dir(this_dir)
-
-    if not SUCCESS:
-        LOGGER.warning('some files are fixed')
-        exit(1)
-
-    LOGGER.info('no file changed')
-
-
 if __name__ == '__main__':
-    fix_all()
+    filepaths = argv[1:]
+    for filepath in filepaths:
+        fix_ten_maru(filepath)
