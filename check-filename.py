2e19df6e5df0f446d1bcce8e41dc5889246a29c2
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-08-21 12:04:25 +0900

    ファイル名のチェック

---
+++ check-filename.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from sys import argv
+import re
+
+FILENAME_REGEX = re.compile(r'^[a-zA-Z0-9./-]*$')
+
+
+def check_filename(filepath: str):
+    if not FILENAME_REGEX.match(filepath):
+        raise ValueError('Invalid filepath: ' + filepath)
+
+
+if __name__ == '__main__':
+    for arg in argv[1:]:
+        check_filename(arg)
