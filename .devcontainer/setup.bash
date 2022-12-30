3dd4c5557f65318c0d86dd0f3f72003b6d6b2d77
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-12-30 12:16:03 +0000

    Use poetry.

--- .devcontainer/setup.bash
+++ .devcontainer/setup.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-pipenv sync --dev
+poetry config virtualenvs.in-project true
+poetry env use 3.10
+poetry install
 
-git config --global --add safe.directory /workspaces/numerical-analysis-note
-pipenv run pre-commit install
+git config --global --add safe.directory $(pwd)
+poetry run pre-commit install
 
 git config gpg.program gpg2
 git config commit.gpgsign true
