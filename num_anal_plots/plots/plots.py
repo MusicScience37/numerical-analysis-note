2b670569cfc0ebc994b1979c98ce545b4ed6bb14
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2023-12-12 15:06:12 +0000

    残りのプロットも PDF を自動生成

--- num_anal_plots/plots/plots.py
+++ num_anal_plots/plots/plots.py
@@ -6,6 +6,12 @@ import dataclasses
 import plotly.graph_objects
 
 from num_anal_plots.plots.laplacian_2d_grid.laplacian_2d_grid import laplacian_2d_grid
+from num_anal_plots.plots.ode_runge_kutta_gmres.ode_runge_kutta_gmres import (
+    ode_runge_kutta_gmres,
+)
+from num_anal_plots.plots.ode_runge_kutta_bicgstab.ode_runge_kutta_bicgstab import (
+    ode_runge_kutta_bicgstab,
+)
 
 
 @dataclasses.dataclass
@@ -18,6 +24,8 @@ class PlotInfo:
 
 PLOT_INFO_LIST = [
     PlotInfo(name="laplacian-2d-grid", figure_factory=laplacian_2d_grid),
+    PlotInfo(name="ode-runge-kutta-gmres", figure_factory=ode_runge_kutta_gmres),
+    PlotInfo(name="ode-runge-kutta-bicgstab", figure_factory=ode_runge_kutta_bicgstab),
 ]
 
 PLOT_NAMES = [info.name for info in PLOT_INFO_LIST]
